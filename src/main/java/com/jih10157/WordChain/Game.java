3cf5f5c971f1b66ffd54ccca7556edba3f50d04c
jih10157 <jih10157@naver.com>
2019-03-18 16:45:19 +0900

    중복 필터 추가

--- src/main/java/com/jih10157/WordChain/Game.java
+++ src/main/java/com/jih10157/WordChain/Game.java
@@ -1,9 +1,17 @@
 package com.jih10157.WordChain;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Game {
+    public final static short NOT_WORD = 1;
+    public final static short SUEC = 0;
+    public final static short FIRST_HANBANG = 2;
+    public final static short NOT_EQUALS = 3;
+    public final static short OVERLAP = 4;
     private static WordManager wordManager;
+    private Set<String> wordSet;
     static {
         try {
             wordManager = new WordManager(new DBCrawling().load());
@@ -16,21 +24,26 @@ public class Game {
     private Game() {
         lastword = "첫 단어를 정해주세요.";
         end = false;
+        wordSet = new HashSet<>();
     }
 
     public String getLastWord() { return lastword; }
-    public boolean nextWord(String str) {
-        if(!wordManager.isWord(str)) return false;
-        if(str.equalsIgnoreCase("첫 단어를 정해주세요.")||!wordManager.isHanbangWord(str)) {
+    public short nextWord(String str) {
+        if(!wordManager.isWord(str)) return NOT_WORD;
+        if(lastword.equalsIgnoreCase("첫 단어를 정해주세요.")) {
+            if(wordManager.isHanbangWord(str)) return FIRST_HANBANG;
             lastword = str;
-            return true;
+            wordSet.add(str);
+            return SUEC;
         }
         if(wordManager.lastandfirst(lastword, str)) {
+            if(wordSet.contains(str)) return OVERLAP;
             lastword = str;
             if (wordManager.isHanbangWord(str)) end = true;
-            return true;
+            wordSet.add(str);
+            return SUEC;
         }
-        return false;
+        return NOT_EQUALS;
     }
     public boolean checkEnd() {
         return end;
--- src/test/java/com/jih10157/WordChain/GameTest.java
+++ src/test/java/com/jih10157/WordChain/GameTest.java
@@ -14,17 +14,30 @@ public class GameTest {
     }
     @Test
     public void getLastWord() {
-        assertTrue(game.nextWord("무지개"));
+        assertEquals(game.nextWord("무지개"), Game.SUEC);
         assertEquals("무지개", game.getLastWord());
     }
-
+    @Test
+    public void nextWord() {
+        assertEquals(game.nextWord("?"), Game.NOT_WORD);
+        assertEquals(game.nextWord("장사꾼"), Game.FIRST_HANBANG);
+        assertEquals(game.nextWord("한"), Game.NOT_WORD);
+        assertEquals(game.nextWord("낱말이아닌것"), Game.NOT_WORD);
+        assertEquals(game.nextWord("띄 어 쓰 기"), Game.NOT_WORD);
+        assertEquals(game.nextWord("기러기"), Game.SUEC);
+        assertEquals(game.nextWord("기러기"), Game.OVERLAP);
+        assertEquals(game.nextWord("기장"), Game.SUEC);
+        assertEquals(game.nextWord("장기"), Game.SUEC);
+        assertEquals(game.nextWord("기장"), Game.OVERLAP);
+        assertEquals(game.nextWord("기러기"), Game.OVERLAP);
+    }
     @Test
     public void checkEnd() {
-        assertTrue(game.nextWord("무지개"));
+        assertEquals(game.nextWord("무지개"), Game.SUEC);
         assertFalse(game.checkEnd());
         game = Game.newGame();
-        assertTrue(game.nextWord("기장"));
-        assertTrue(game.nextWord("장사꾼"));
+        assertEquals(game.nextWord("기장"), Game.SUEC);
+        assertEquals(game.nextWord("장사꾼"), Game.SUEC);
         assertTrue(game.checkEnd());
     }
 
